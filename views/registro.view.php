b8ece810dd5661c9563aff49662c40101ea9981e
blastoken <blastoken@gmail.com>
2020-02-23 17:15:33 +0100

    modificación puntual registro

--- views/registro.view.php
+++ views/registro.view.php
@@ -21,7 +21,7 @@ if(isset($_SESSION['erroresRegistro'])){
       <form method="post">
           <input type="text" id="nombre" name="nombre" class="form-control text-center mb-1" placeholder="Nombre" value=<?php if(isset($_SESSION['formularioRegistro'])){echo $_SESSION['formularioRegistro']['nombre'];}?>>
           <input type="text" id="apellidos" name="apellidos" class="form-control text-center mb-1" placeholder="Apellidos" value=<?php if(isset($_SESSION['formularioRegistro'])){echo $_SESSION['formularioRegistro']['apellidos'];}?>>
-          <input type="text" id="usuario" name="usuario" class="form-control text-center mb-1" placeholder="Usuario (email)" value=<?php if(isset($_SESSION['formularioRegistro'])){echo $_SESSION['formularioRegistro']['usuario'];}?>>
+          <input type="text" id="usuario" name="usuario" class="form-control text-center mb-1" placeholder="Usuario" value=<?php if(isset($_SESSION['formularioRegistro'])){echo $_SESSION['formularioRegistro']['usuario'];}?>>
           <input type="password" id="pass" name="pass" class="form-control text-center mb-1" placeholder="Contraseña" value=<?php if(isset($_SESSION['formularioRegistro'])){echo $_SESSION['formularioRegistro']['pass'];}?>>
           <input type="password" id="pass2" name="pass2" class="form-control text-center mb-1" placeholder="Repite contraseña" value=<?php if(isset($_SESSION['formularioRegistro'])){echo $_SESSION['formularioRegistro']['pass2'];}?>>
           <input id="registrarse" type="submit" name="registrarse" value="Registrarse"  class="btn btn-primary w-100 mt-1 blancoTexto"/>
